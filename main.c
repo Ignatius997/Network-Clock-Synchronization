951eb5569bd95cf1f2313db0ef84ef01c0f02d1c
Ignacy Pernach <if.pernach@student.uw.edu.pl>
2025-04-22 12:06:30 +0200

    save2

--- main.c
+++ main.c
@@ -1,33 +1,50 @@
 #include <stdint.h>
-#include <arpa/inet.h> // TODO: sugerowane niby netinet/in, ale na labach jest arpa.
+#include <arpa/inet.h>
 #include <stdbool.h>
 #include <regex.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define DEFAULT_PORT 0
+
 #define ARGOPT_BIND_ADDRESS 'b'
 #define ARGOPT_PORT         'p'
 #define ARGOPT_PEER_ADDRESS 'a'
 #define ARGOPT_PEER_PORT    'r'
 
+#define MSG_HELLO       1
+#define MSG_HELLO_REPLY 2
+#define MSG_CONNECT     3
+#define MSG_ACK_CONNECT 4
+
+#define MSG_SYNC_START     11
+#define MSG_DELAY_REQUEST  12
+#define MSG_DELAY_RESPONSE 13
+
+#define MSG_LEADER 21
+
+#define MSG_GET_TIME 31
+#define MSG_TIME     32
+
 /** Node attributes shall be accessible via global variables.
  * Naming convention: g_{name}.
  */
+static int g_socket_fd;
 
 // ---- Program arguments parsing ---- //
 
 typedef struct ProgramArgs {
-    uint8_t     *bind_address;
+    char        *bind_address;
     uint16_t    port;
-    uint8_t     *peer_address;
+    char        *peer_address;
     uint16_t    peer_port;
     
     bool        _ar_provided;
-    uint8_t     _peer_address_length; // TODO Chyba niepotrzebne.
-
 } ProgramArgs;
 
 ProgramArgs args_default() {
@@ -38,7 +55,6 @@ ProgramArgs args_default() {
         .peer_port      = DEFAULT_PORT,
     
         ._ar_provided           = false,
-        ._peer_address_length   = 0
     };
 }
 
@@ -103,7 +119,6 @@ void args_load_value(char *arg, const char opt, ProgramArgs *program_args) {
 
         case ARGOPT_PEER_ADDRESS:
             program_args->peer_address = arg;
-            program_args->_peer_address_length = get_ip_length(arg);
             break;
 
         case ARGOPT_PEER_PORT:
@@ -180,10 +195,16 @@ typedef struct Message {
     uint8_t     synchronized;
 } Message;
 
-inline Message msg_hello(ProgramArgs args) {
-    return (Message) {
-        .message = 1,
-    };
+Message msg_init() {
+    Message msg;
+    memset(&msg, 0, sizeof(Message));
+    return msg;
+}
+
+Message msg_hello() {
+    Message msg = msg_init();
+    msg.message = MSG_HELLO;
+    return msg;
 }
 
 inline Message msg_hello_reply() {
@@ -198,9 +219,77 @@ inline Message msg_acknowledge_connect() {
 
 }
 
+// TODO przeniesc w inne miejsce
+/**
+ * Zajumane z labów udp: funkcja `get_server_address`.
+ */
+static struct sockaddr_in get_peer_address(char const *peer, uint16_t port) {
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    struct addrinfo *address_result;
+    int errcode = getaddrinfo(peer, NULL, &hints, &address_result);
+    if (errcode != 0) {
+        syserr("getaddrinfo"); // OG: `fatal("getaddrinfo: %s", gai_strerror(errcode));`
+    }
+
+    struct sockaddr_in send_address;
+    send_address.sin_family = AF_INET;  // IPv4
+    send_address.sin_addr.s_addr =      // IP address
+        ((struct sockaddr_in *) (address_result->ai_addr))->sin_addr.s_addr;
+    send_address.sin_port = htons(port);
+
+    freeaddrinfo(address_result);
+    return send_address;
+}
+
 void notify_peer(ProgramArgs args) {
-    Message msg_hello = msg_hello(args);
-    
+    Message msg_hello = msg_hello();
+    // Wyślij hello
+    struct sockadr_in peer_address = get_peer_address(args.peer_address, args.peer_port);
+    char const *peer_ip = inet_ntoa(peer_address.sin_addr);
+    uint16_t peer_port = ntohs(peer_address.sin_port);
+    connect_
+
+    // Odbierz komunikat HELLO_REPLY
+    for (address in reply.peer_address) {
+        connect with address:reply.peer_port; // with msg_connect
+    }
+    // Czy to można rzeczywiście rozbić na dwie pętle?
+    for (address in reply.peer_address) {
+        receive msg_ack_connect;
+        dodaj do listy wezlow potwierdzony wezel;
+    }
+}
+
+// TODO przenieść w inne miejsce
+void init_addr(struct sockaddr_in *bind_address, uint16_t port) {
+    // NOTE kod i komentarz zajumany z echo-server.c z labów udp.
+    // Bind the socket to a concrete address
+    bind_address->sin_family = AF_INET; // IPv4
+    bind_address->sin_addr.s_addr = htonl(INADDR_ANY);
+    bind_address->sin_addr = htons(port);
+}
+
+// TODO przenieść w inne miejsce
+// TODO lepsza nazwa
+// TODO rozbić socket z bind_address?
+void init_socket(struct sockaddr_in *bind_address, uint16_t port) {
+    g_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (g_socket_fd < 0) {
+        syserr("Socket creation failed");
+    }
+
+    init_addr(bind_address, port);
+
+    if (bind(g_socket_fd, (struct sockaddr *) bind_address, (socklen_t) sizeof(*bind_address)) < 0) {
+        syserr("bind");
+    }
+
+    fprintf(stderr, "Listening on port %" PRIu16 "\n", port);
 }
 
 void join_network(ProgramArgs args) {
@@ -215,6 +304,9 @@ int main(int argc, char* argv[]) {
     ProgramArgs program_args = args_default();
     args_parse(argc, argv, &program_args);
 
+    struct sockaddr_in bind_address; // To avoid allocation on the stack.
+    init_socket(&bind_address, program_args.port);
+    
     join_network(program_args);
     
     return 0;
